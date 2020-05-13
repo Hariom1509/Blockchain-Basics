933d67a3c9a7336c601e408aa5946f0dcb195724
Hariom Vyas <u19ee014@eed.svnit.ac.in>
2020-05-13 16:02:05 +0530

    Create Bidder.sol

---
+++ Bidder.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.5.1;
+
+contact Bidder
+{
+    string public name = "Bidder";
+    string public bidamount;
+    string public eligible;
+    uint constant minamount = 1000;
+    
+    function setName(string nm) public
+    {
+        name = nm;
+    }
+    
+    function setBidamount(uint x) public
+    {
+        bidamount = x;
+    }
+    
+    function determineEligibility() public
+    {
+        if(bidamount >= x)
+        eligible = true;
+        else
+        eligible = false;
+    }
+}
